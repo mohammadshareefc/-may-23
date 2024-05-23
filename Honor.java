72a6e4167984f1d9e8f6d495107f6bf2d7a6d513
mohammadshareefc <shareef.chilakala16@gmail.com>
2024-05-23 11:46:38 +0530

    M

---
+++ Honor.java
@@ -0,0 +1,11 @@
+public class Honor {
+    public static void main(String[] args) {
+        System.out.println(3+5);
+        System.out.println(8+7);
+
+        int a=3;
+        int b=5;
+        int result=a+b;
+        System.out.println(result);
+    }
+}
